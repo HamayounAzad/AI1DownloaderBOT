df7bef3d5497c0e71a875ed9bfde292e26f28cfa
Mohammad Hamayoun Aazd <112551352+HamayounAzad@users.noreply.github.com>
2025-12-31 14:15:06 +0430

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+token = "8457133468:AAHTyaFeD-DA1vrDbroIo2IgD1pylj8B3rY"  # telegram token
+logs =   # logs channel id, if none set to None
+max_filesize = 500000000  # bytes
+output_folder="/tmp/yt-dlp-telegram"
